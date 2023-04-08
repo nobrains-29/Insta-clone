7026d5544c41c886bb9cf12c08f71e0b327e6646
Archit Tripathi <archittripathi94@gmail.com>
2023-04-09 02:07:27 +0530

    nit

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import M from "materialize-css";
 
 const Signup = () => {
